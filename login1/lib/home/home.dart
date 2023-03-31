a78fbf29883c419f913c7892b679412dc0475091
gulzigit22 <gulzigittalant@gmail.com>
2023-03-31 16:41:21 +0600

    images

--- login1/lib/home/home.dart
+++ login1/lib/home/home.dart
@@ -6,13 +6,24 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          Text(
-            'data',
-            style: TextStyle(fontSize: 100),
-          )
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    color: Colors.blue,
+                    width: 400,
+                    height: 500,
+                    child: CircleAvatar(
+                        radius: 1,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(200),
+                            child: Image.asset('assets/apple.jpg'))))
+              ],
+            ),
+          ),
         ],
       ),
     );
